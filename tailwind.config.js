8589b0475169feeea294e3b79bba48a5b2f602ce
enmotion <40427163@qq.com>
2022-06-22 01:36:41 +0800

    TEST TAILWINDS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors');
-function ganerateAlphaStepColor(key='ad',color='#FF0000',step=10){
+function ganerateAlphaStepColor(key='a',color='#FF0000',step=10){
   var obj={}
-  for(let i=1;i<=step;i++){
+  for(let i=1 ; i <= step ; i++){
     obj[key+''+i] = color + (Math.max(Math.round(256/step*i-1),0).toString(16))
   }
   return obj
@@ -12,11 +12,13 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors:{
-      d:ganerateAlphaStepColor('a','#FF0000',10),
-      s:ganerateAlphaStepColor('a','#FF0000',10),
-      w:ganerateAlphaStepColor('a','#FF0000',10),
+      d:ganerateAlphaStepColor('a','#D92211',10),
+      s:ganerateAlphaStepColor('a','#59D986',10),
+      w:ganerateAlphaStepColor('a','#F2C849',10),
       i:ganerateAlphaStepColor('a','#FF0000',10),
-      p:ganerateAlphaStepColor('a','#FF0000',10),
+      p:ganerateAlphaStepColor('a','#2C6CBF',10),
+      black:ganerateAlphaStepColor('a','#000000',24),
+      white:ganerateAlphaStepColor('a','#FFFFFF',24),
       blue:colors.blue
     },
     spacing:function(baseStep=5,domain=100,step=0,maxstep=100){
