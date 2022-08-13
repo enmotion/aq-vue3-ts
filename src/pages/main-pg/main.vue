ffcac104d273f0831f24f02a79af9ffa0c1d5e8d
xuli <xuli@op-mobile.com.cn>
2022-08-13 18:04:50 +0800

    scroll view

--- src/pages/main-pg/main.vue
+++ src/pages/main-pg/main.vue
@@ -3,14 +3,14 @@
     <div class="h-140 bg-p-6 xrow">
       <div class="w-200 xcol">
         <!-- logo部分 -->
-        <div class="h-100 items-center xrow justify-center px-10">
+        <div class="h-90 items-center xrow justify-center px-0">
           <span class="iconfont icon-unicom-logo text-4xl text-white"></span>
-          <span class="ml-10 xcol items-start justify-center" style="margin-top:2px">
-            <span class="text-sm text-white font-bold" style="line-height:14px">纬业流程</span>
-            <span class="h-15 text-xs text-light-32 uppercase font-bold scale-75 origin-bottom-left" style="line-height:12px">version:0.0.1</span>
+          <span class="ml-10 xrow items-start justify-center" style="margin-top:2px">
+            <span class="text-lg text-white font-bold" style="line-height:14px">纬业流程</span>
+            <span class="h-15 text-xs text-light-32 uppercase font-bold origin-bottom-left ml-5" style="line-height:12px;padding-top:4px">0.0.1</span>
           </span>
         </div>
-        <div class="h-40">
+        <div class="h-50">
         </div>
       </div>
       <!-- 导航与标签 -->
@@ -29,21 +29,21 @@
           </aq-click-scroll-view>
         </div>
         <!-- tab部分 -->
-        <div class="h-50 xrow items-end">
-          <div class="h-40 w-auto flex-grow-1 xrow items-end bg-dark-12 border-t border-light-6 rounded-t-md backdrop-blur-lg">
-            <aq-click-scroll-view class="h-50 xrow">
-              <span class="h-50 xrow items-end">
+        <!-- <div class="h-50 xrow items-end">
+          <div class="h-50 w-auto flex-grow-1 xrow items-end bg-dark-6 border-t border-light-6 backdrop-blur-lg">
+            <aq-click-scroll-view class="h-55 xrow">
+              <span class="h-55 xrow items-end">
                 <span v-for="(i) in 20" 
                   :key="i" 
                   class="px-15 flex-shrink-0 text-xs font-bold rounded-t-md mr-5 last:mr-0 xcol items-center justify-center"
-                  :class="[i==2?'h-45 bg-gradient-to-t from-gray-200 to-white text-gray-600':
-                  'text-light-18 h-40 cursor-pointer hover:h-45 hover:bg-p-10 hover:text-white transition-all duration-300']">
+                  :class="[i==2?'h-55 bg-gradient-to-t from-gray-200 to-white text-gray-600':
+                  'text-light-18 h-40 cursor-pointer hover:h-55 hover:bg-p-10 hover:text-white transition-all duration-300']">
                     标签菜单-{{i}}
                 </span>
               </span>
             </aq-click-scroll-view>
           </div>
-        </div>
+        </div> -->
       </div>
       <!-- 个人菜单 -->
       <div class="bg-dark-8 border-l border-light-6 xcol px-20 py-10 backdrop-blur-lg">
@@ -51,7 +51,7 @@
           <span class="h-45 w-45 rounded-full flex-shrink-0 flex-grow-0 bg-white  overflow-hidden shadow-lg shadow-white">
             <img src="http://i1.go2yd.com/image.php?url=0W8wDjc3Sh" class="rounded-full" style="transform:scale(0.97)" alt="">
           </span>
-          <span class="xrow w-100 flex-wrap ml-15 text-white flex-grow-1 overflow-hidden" style="margin-top: 3px;">
+          <span class="xrow w-120 flex-wrap ml-15 text-white flex-grow-1 overflow-hidden" style="margin-top: 3px;">
             <span class="w-full text-sm text-left h-15 font-bold overflow-hidden text-ellipsis break whitespace-nowrap" style="line-height:14px">
               叶文洁
             </span>
@@ -86,7 +86,7 @@
           <div class="xcol mt-20">
             <span v-for="(i) in 8" :key="i" 
               class="h-40 xrow items-center overflow-hidden mb-10 last:mb-0"
-              :class="[i==4?'bg-p-7 rounded-lg px-5':'']">
+              :class="[i==4?'bg-p-10 rounded-lg px-5':'']">
               <span 
                 class="h-20 w-20 iconfont icon-flow ml-5"
                 :class="[i==4?'text-white text-lg':'text-gray-500 text-sm']" 
